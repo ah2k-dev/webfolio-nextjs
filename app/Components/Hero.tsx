3a667465f7fd0a5b2839042c98fedb1f00afdee2
Ahmed Hassan <ah2k.dev@gmail.com>
2024-08-10 01:21:57 +0500

    trying stars bg

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -9,6 +9,12 @@ const Hero = ({
 }>) => {
   return (
     <section className="parallax">
+      <div className="bg-animation">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="stars4"></div>
+      </div>
       {/* <div className="parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10">
         <div className="flex-1 lg:mb-0">
           <h1 className="font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]">
@@ -24,13 +30,13 @@ const Hero = ({
         </div>
       </div> */}
 
-      <Image
+      {/* <Image
         src="/assets/parallax/1Stars.svg"
         alt=""
         className="parallax__stars"
         width={200}
         height={200}
-      />
+      /> */}
       <Image
         src="/assets/parallax/2Planets.svg"
         alt=""
@@ -59,13 +65,13 @@ const Hero = ({
         height={200}
         width={200}
       />
-      <Image
+      {/* <Image
         src="/assets/parallax/6Sun.svg"
         alt=""
         className="parallax__sun"
         height={200}
         width={200}
-      />
+      /> */}
 
       {/* <SpacemanCanvas scrollContainer={scrollContainer} /> */}
     </section>
